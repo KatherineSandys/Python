b039efa3233d5ad8211d63bc0c4779d2c945eac6
KatherineSandys <katherinesandys@gmail.com>
2020-08-17 21:06:29 -0400

    Moved file

---
+++ Web Page Reloader/OverLoad.py
@@ -0,0 +1,27 @@
+import pynput
+from time import sleep
+from pynput.keyboard import Key, Controller
+
+#set keyboard
+keyboard = Controller()
+
+#get user input on how long to sleep for
+TSleep = float(input("Enter how long between each enter click in milliseconds: "))
+TSleep = TSleep * 0.001 #getting to milliseconds for sleep to use
+#get how long program should run for
+run = float(input("How long do you want to run this program for in minutes: "))
+run = run * 60 #getting to seconds
+
+#figure out how many times to loop
+loop = run / TSleep
+
+sleep(5) #waits for user to get on the page
+print("GO")
+
+for x in range(0, int(loop)): #loops for hower long the user says to
+    keyboard.press(Key.enter)
+    sleep(TSleep / 2)
+    keyboard.release(Key.enter)
+    sleep(TSleep / 2)
+
+print("END")
